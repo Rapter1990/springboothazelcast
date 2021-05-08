87a8f480f8e81e72e90a0c37ff800f55c336dc16
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-08 13:29:13 +0300

    Task 1 : Initial commit

---
+++ src/test/java/com/springboot/hazelcast/HazelcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.hazelcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazelcastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
